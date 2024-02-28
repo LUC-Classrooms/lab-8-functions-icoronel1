4dc00d9d11e80cbf5d683beb46462015539981d1
Isa Coronel <156938989+icoronel1@users.noreply.github.com>
2024-02-28 17:48:26 +0000

    Kirby Multi-verse

--- script.js
+++ script.js
@@ -6,7 +6,12 @@ function draw() {
   background(200);
 
   //this is a call to myShape()
-  myShape(width / 2, height / 2, 1);
+  myShape(width / 2, height / 2, 1, 1);
+  myShape(width/3, height/3, 2);
+  myShape(width * 2/3, height * 2/3, .5)
+
+  for(let i = 0; i < 10; i++)
+    myShape(i * 50 + 30, 50, .5, i)
   // myShape() takes three arguments
   // arg1: horizontal location
   // arg2: vertical location
@@ -16,13 +21,30 @@ function draw() {
   
 }
 
-function myShape(x, y, s) {
+function myShape(x, y, s, a) {
   // make this function more interesting
   push(); // make a separate layer
   translate(x, y); // move the origin point
   scale(s);
-  
-  ellipse(0, 0, w, h); // simple ellipse at the translated origin (0,0)
+  rotate(a);
+  fill("pink")
+  ellipse(0, 0, 50, 50); // simple ellipse at the translated origin (0,0)
+  arc(20, 8, 20, 10, PI + HALF_PI, HALF_PI) // right arm
+  arc(-20, 8, 20, 10, HALF_PI, PI + HALF_PI) // left arm
+  fill("red")
+  ellipse(-10, 20, 10, 20) // left foot
+  ellipse(10, 20, -10, 20) // right foot
+  ellipse(-15, -5, 8, 3) // left blush
+  ellipse(15, -5, 8, 3) // right blush
+  fill(0)
+  ellipse(-6, -6, 5, 15) // left eye
+  ellipse(6, -6, 5, 15) // right eye
+  fill(255)
+  ellipse(6, -8, 4, 8) // right eye shine
+  ellipse(-6, -8, 4, 8) // left eye shine
+  noFill()
+  arc(.5, 0, 4, 4, TWO_PI, PI) // mouth
+
   
   pop(); // dispose of the layer
 }
